041eab9c823e0df178116589d8d69c1dc89e6fd6
nigussolomon <nigussolomon444@gmail.com>
2023-03-04 23:58:15 +0300

    addes rest_framework to project

--- SIS/settings.py
+++ SIS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$i8f$+*rlst-og41h-40pu-4o*2*tje!pt)x7psr=vb1!ry)-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'core'
 ]
 
