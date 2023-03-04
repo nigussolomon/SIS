de448352eac2a886c72c216d252280f194fe588c
nigussolomon <nigussolomon444@gmail.com>
2023-03-04 17:43:01 +0300

    created a custome user model and added needed functionalities

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import SisUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class UserAdminConfig(UserAdmin):
+    search_fields = ('email',)
+    list_filter = ('email', 'is_active', 'is_staff', 'is_superuser', 'user_type')
+    list_editable = ('is_active', 'is_staff', 'is_superuser', 'user_type')
+    ordering = ('email',)
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser' ,'user_type')
+    fieldsets = (
+        (None, {'fields': ('email', 'user_type', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_type', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(SisUser, UserAdminConfig)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+USER_TYPES = [
+    ("Student", "STUDENT"),
+    ("Instructor", "INSTRUCTOR"),
+    ("Admin", "ADMIN"),
+    ("Other Staff", "OTHER STAFF"),
+]
+
+
+class CustomAccMan(BaseUserManager):
+    def create_superuser(self, email, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+        user_type = USER_TYPES[2][0]
+        if other_fields.get('is_staff') is not True:
+             raise ValueError("SuperUser must be set to is_staff=True")
+        
+        if other_fields.get('is_superuser') is not True:
+             raise ValueError("SuperUser must be set to is_superuser=True")
+             
+        
+        return self.create_user(email, password, user_type, **other_fields)
+    
+    def create_user(self, email, password, user_type, **other_fields):
+        if not email:
+            raise ValueError("Please provide a valid email address")
+        if user_type is None:
+            user_type = USER_TYPES[0][0]
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_type=user_type ,**other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+class SisUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    user_type = models.CharField(max_length=30, choices=USER_TYPES)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomAccMan()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
